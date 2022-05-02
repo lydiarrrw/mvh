c5422d3bd3cf444a898ab50527c60e92ab4c6465
Lydia <lydiaawood@googlemail.com>
2022-05-02 12:05:37 +0100

    initial design

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 // ? Libraries
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import 'bulma'
-import './styles/style.scss'
+import Home from "./components/Home.js";
+import Nav from "./components/Nav.js";
+import Footer from "./components/Footer.js";
+import WhatsOn from "./components/WhatsOn.js";
+import About from "./components/About.js";
+import Book from "./components/Book.js";
 
-const App = () => {
+import "./styles/styles/style.scss";
 
-  return <section>
-    <h1>Milburn Village Hall</h1>
-    <p>Website under maintenance</p>
-    <p>Please check back soon for updates</p>
+const App = () => (
+  <BrowserRouter>
+    <Nav />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/whats-on" component={WhatsOn} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/book" component={Book} />
+    </Switch>
+    <Footer />
+  </BrowserRouter>
+);
 
-  </section>
-}
-
-export default App
+export default App;
